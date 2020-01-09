04b800ef037831f518594b6c1e44700798a4d792
Ella <ewickstrom@bsd220.org>
2020-01-09 10:40:53 -0600

    Initial Commit

---
+++ TicTacToe/GridLabel.swift
@@ -0,0 +1,15 @@
+//
+//  GridLabel.swift
+//  TicTacToe
+//
+//  Created by Ella Wickstrom on 11/18/19.
+//  Copyright © 2019 Ella Wickstrom. All rights reserved.
+//
+
+import UIKit
+
+class GridLabel: UILabel {
+
+    var canTap = true
+
+}
